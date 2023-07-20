db1578fe28613d9248d8b11f5dcc97cbe022e4c0
Lurimago <luisricardomarcelogomez@gmail.com>
2023-07-20 12:11:32 -0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>TE AMO, NO SEÑORA RENEGONA</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
